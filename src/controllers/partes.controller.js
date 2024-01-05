62e8c4e364a777c5f3a6fea54fd9990b718a1dcb
santilapi13 <santilapiana02@gmail.com>
2024-01-05 13:02:58 -0300

    fix: partesService.createParte dentro del bloque try

--- src/controllers/partes.controller.js
+++ src/controllers/partes.controller.js
@@ -13,15 +13,16 @@ async function getPartes(req, res) {
 async function postParte(req, res) {
     let { remolcador, buque, maniobra, hora_inicio, hora_fin, solicitante, bandera, observaciones, practico, otra_embarcacion } = req.body;
     let parte;
+    let result;
 
     try {
         parte = new ParteDTO({ remolcador, buque, maniobra, hora_inicio, hora_fin, solicitante, bandera, observaciones, practico, otra_embarcacion });
         await parte.validatePropertiesValues();
+        result = await partesService.createParte(parte);
     } catch (error) {
         return res.sendBadRequestError(error.message);
     }
 
-    let result = await partesService.createParte(parte);
     res.sendCreated(result);
 }
 
