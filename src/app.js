d1134d265a9536e046a064c5e1436c2877771ab3
santilapi13 <santilapiana02@gmail.com>
2024-02-26 17:21:32 -0300

    fix: cors origin

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 initializePassport();
 app.use(passport.initialize());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://administracion-puerto.vercel.app/',
     credentials: true,
 }));
 
